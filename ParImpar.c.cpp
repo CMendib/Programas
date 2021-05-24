d5789bca024ef48c3fba65b7877e02dc779322fc
AaronCortes22 <82549967+AaronCortes22@users.noreply.github.com>
2021-05-23 23:14:37 -0500

    Add files via upload

---
+++ ParImpar.c.cpp
@@ -0,0 +1,19 @@
+/*Dep3_Prog3:
+Verificar si es par e impar*/
+#include <stdio.h>
+
+int main ()
+{
+    int numero;
+    printf("\n Introduce un numero");
+    scanf("%d",&numero);
+
+    if(numero%2 == 0)
+    {
+    printf("\n El numero es PAR");
+    }
+    else
+    {
+    printf("\n ");
+    }
+}
